b8896c97fdc224638b49e73cf017aad03d44d650
Prakash Barik <badge2prakash@gmail.com>
2019-10-30 13:19:20 +0530

    added screenshot method

--- src/main/java/com/guru99/utility/Screenshots.java
+++ src/main/java/com/guru99/utility/Screenshots.java
@@ -1,5 +1,32 @@
 package com.guru99.utility;
 
-public class Screenshots {
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class Screenshots {
+	String filePath = "E:\\workspace\\Guru99Ecommerce\\Screenshot\\";
+	WebDriver driver;
+	
+	public void takeScreenshot() {
+		TakesScreenshot scr = ((TakesScreenshot)driver);
+		File srcFile = scr.getScreenshotAs(OutputType.FILE);
+		String timeStamp = new SimpleDateFormat("dd_MM_yyyy__hh_mm_ss").format(new Date());
+		File destFile = new File(filePath+"screenshot"+timeStamp+".png");
+		try 
+		{
+			FileUtils.copyFile(srcFile, destFile);
+		} 
+		catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
